76bb97ded9d47e2dc6f8c4f178a054cd73c2887c
Obi Chinedu Frank <frankieetchy@gmail.com>
2023-05-11 23:41:31 +0100

    add get clip route

--- src/lib/domain/clip/field/shortcode.rs
+++ src/lib/domain/clip/field/shortcode.rs
@@ -51,6 +51,16 @@ impl From<&str> for ShortCode {
     }
 }
 
+use rocket::request::FromParam;
+
+impl<'r> FromParam<'r> for ShortCode {
+    type Error = &'r str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        Ok(ShortCode::from(param))
+    }
+}
+
 impl FromStr for ShortCode {
     type Err = ClipError;
 
